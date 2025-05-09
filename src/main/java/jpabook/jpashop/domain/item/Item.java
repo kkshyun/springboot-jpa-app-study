f5a1bfe6ccc9992aaa21962e51aed9ed1d554428
sehyunnnn <kkshyun56@gmail.com>
2025-05-09 23:20:16 +0900

    [fix] : section3

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+// JPA에서 객체의 상속 구조를 DB 테이블에 어떻게 매핑할지 결정할 때 사용
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한 테이블에 다 넣음
 @DiscriminatorColumn(name = "dtype")
 @Getter @Setter
