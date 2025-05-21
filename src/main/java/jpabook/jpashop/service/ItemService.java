b83dc67d8e6a33b592bcc53880be64ae55b1bbd9
sehyunnnn <kkshyun56@gmail.com>
2025-05-22 02:16:44 +0900

    [feat] : section 8

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -18,7 +18,22 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-    public List<Item> findItem() {
+    @Transactional
+    // @Transactional 에 의해서 커밋이 되면 JPA는 flush를 날림 변경된 애를 찾아서 DB에 update 쿼리를 침
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        // 이런식으로 set을 다 하는게 아니라 메서드를 만드는게 좋음
+        // 병합하는 방법보다 변경 감지
+//        findItem.setPrice(param.getPrice());
+//        findItem.setName(param.getName());
+//        findItem.setStockQuantity(param.getStockQuantity());
+//        itemRepository.save(findItem);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
